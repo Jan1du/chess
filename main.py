196dbb2abb7b5d847e9866d608baade8fd79fe31
Jan1du <149525402+Jan1du@users.noreply.github.com>
2023-12-03 13:48:29 +0000

    main.py

--- main.py
+++ main.py
@@ -6,30 +6,31 @@ pygame.init()
 #--colors--
 white = (255,255,255)
 grey = (100,100,100)
+grey_light = (150,150,150)
 black = (0,0,0)
 
-
 #--constants--
-SCREEN_WIDTH, SCREEN_HEIGHT = 400, 600
-LOGO_WIDTH, LOGO_HEIGHT = 350, 200
-BUTTON_WIDTH, BUTTON_HEIGHT = 300, 75
-font = pygame.font.Font("freesansbold.ttf", 40)
+screen_width, screen_height = 400, 600
+logo_width, logo_height = 350, 200
+button_width, button_height = 250, 75
+montserrat = "fonts/Montserrat/Montserrat-ExtraBold.ttf"
+font = pygame.font.Font(montserrat, 37)
 
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])  
+screen = pygame.display.set_mode([screen_width, screen_height])  
 pygame.display.set_caption("chess")
 
 #--assets--
 logo = pygame.image.load(os.path.join("chess assets", "Chess logo.png"))
-logo = pygame.transform.scale(logo, (LOGO_WIDTH, LOGO_HEIGHT))   
+logo = pygame.transform.scale(logo, (logo_width, logo_height))   
 
 
 def draw_buttons():
-    play_button = pygame.draw.rect(screen, white, pygame.Rect(50, 350, BUTTON_WIDTH, BUTTON_HEIGHT))
-    learn_button = pygame.draw.rect(screen, white, pygame.Rect(50, 450, BUTTON_WIDTH, BUTTON_HEIGHT))
+    play_button = pygame.draw.rect(screen, white, pygame.Rect(75, 350, button_width, button_height), 0, 10)
+    learn_button = pygame.draw.rect(screen, white, pygame.Rect(75, 450, button_width, button_height), 0, 10)
     text_play = font.render("Play", True, black)
     text_learn = font.render("Learn", True, black)
-    screen.blit(text_play, (157,370))
-    screen.blit(text_learn, (145,470))
+    screen.blit(text_play, (157,365))
+    screen.blit(text_learn, (145,465))
     return play_button, learn_button
 
 
